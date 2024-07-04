020602e9db5984b2eefb27a87d51e5402b1ae412
Alexis Lecanu <alexis.lecanu@gmail.com>
2024-07-04 14:01:37 +0200

    Feature #425: add settings on item quest + change filtrer if enable
    # Conflicts:
    #	app/src/main/res/values/strings_ee.xml

--- app/src/main/java/de/westnordost/streetcomplete/quests/crossing_markings/AddCrossingMarkings.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/crossing_markings/AddCrossingMarkings.kt
@@ -1,5 +1,7 @@
 package de.westnordost.streetcomplete.quests.crossing_markings
 
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.data.elementfilter.toElementFilterExpression
 import de.westnordost.streetcomplete.data.osm.geometry.ElementGeometry
@@ -10,18 +12,19 @@ import de.westnordost.streetcomplete.data.user.achievements.EditTypeAchievement.
 import de.westnordost.streetcomplete.osm.Tags
 import de.westnordost.streetcomplete.osm.isCrossing
 
+private const val PREF_CROSSING_MARKING_EXTENDED="quest_pedestrian_crossing_markings_extended"
 class AddCrossingMarkings : OsmElementQuestType<CrossingMarkings> {
 
-    private val crossingFilter by lazy {
-        """
+    private val crossingFilter
+        get() =
+            """
         nodes with
           highway = crossing
           and foot != no
-          and !crossing:markings
-          and (!crossing or crossing = island)
+          and $crossingMarkingExpression
           and (!crossing:signals or crossing:signals = no)
     """.toElementFilterExpression()
-    }
+
     /* only looking for crossings that have no crossing=* at all set because if the crossing was
      * - if it had markings, it would be tagged with "marked","zebra" or "uncontrolled"
      * - if it hadn't, it would be tagged with "unmarked"
@@ -60,7 +63,7 @@ class AddCrossingMarkings : OsmElementQuestType<CrossingMarkings> {
         if (!crossingFilter.matches(element)) false else null
 
     override fun createForm() =
-        if (prefs.getBoolean("quest_pedestrian_crossing_markings_extended", false)) {
+        if (isCrossingMarkingExtended) {
             AddCrossingMarkingsForm()
         } else {
             AddCrossingMarkingsYesNoForm()
@@ -78,4 +81,33 @@ class AddCrossingMarkings : OsmElementQuestType<CrossingMarkings> {
          * which kinds exist per country needs research. (Whose results should be added to the
          * wiki page for crossing:markings first) */
     }
+    override val hasQuestSettings: Boolean = true
+
+    override fun getQuestSettingsDialog(context: Context): AlertDialog =
+        AlertDialog.Builder(context)
+            .setMessage(R.string.pref_quest_pedestrian_crossing_markings_extended)
+            .setPositiveButton(R.string.quest_generic_hasFeature_yes) { _, _ ->
+                prefs.edit().putBoolean(PREF_CROSSING_MARKING_EXTENDED,true).apply()
+            }
+            .setNegativeButton(R.string.quest_generic_hasFeature_no) { _, _ ->
+                prefs.edit().putBoolean(PREF_CROSSING_MARKING_EXTENDED,false).apply()
+            }
+            .create()
+
+    private val isCrossingMarkingExtended
+        get() = prefs.getBoolean("quest_pedestrian_crossing_markings_extended", false)
+
+    private val crossingMarkingExpression: String
+        get() =
+            if (isCrossingMarkingExtended) {
+                """
+                    ( !crossing:markings or crossing:markings=yes )
+                """.trimIndent()
+            } else {
+                """
+                    !crossing:markings
+                    and (!crossing or crossing = island )
+                """.trimIndent()
+            }
+
 }
