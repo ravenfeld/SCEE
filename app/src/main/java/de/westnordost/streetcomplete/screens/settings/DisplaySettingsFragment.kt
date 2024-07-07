2e3c43856813e2363519c28076746b7313731a1b
Alexis Lecanu <alexis.lecanu@gmail.com>
2024-07-07 08:08:47 +0200

    Fix import gpx

--- app/src/main/java/de/westnordost/streetcomplete/screens/settings/DisplaySettingsFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/settings/DisplaySettingsFragment.kt
@@ -15,26 +15,18 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.russhwolf.settings.ObservableSettings
-import de.westnordost.streetcomplete.ApplicationConstants.MAX_DOWNLOADABLE_AREA_IN_SQKM
 import de.westnordost.streetcomplete.Prefs
 import de.westnordost.streetcomplete.R
 import de.westnordost.streetcomplete.StreetCompleteApplication
 import de.westnordost.streetcomplete.data.download.DownloadController
 import de.westnordost.streetcomplete.data.download.DownloadWorker
-import de.westnordost.streetcomplete.data.download.tiles.TilePos
-import de.westnordost.streetcomplete.data.download.tiles.enclosingTilePos
-import de.westnordost.streetcomplete.data.download.tiles.upToTwoMinTileRects
 import de.westnordost.streetcomplete.data.importGpx
 import de.westnordost.streetcomplete.data.osm.mapdata.LatLon
 import de.westnordost.streetcomplete.data.visiblequests.VisibleQuestTypeController
 import de.westnordost.streetcomplete.screens.HasTitle
 import de.westnordost.streetcomplete.util.dialogs.setViewWithDefaultPadding
 import de.westnordost.streetcomplete.util.ktx.toast
-import de.westnordost.streetcomplete.util.logs.Log
-import de.westnordost.streetcomplete.util.math.area
-import de.westnordost.streetcomplete.util.math.contains
-import de.westnordost.streetcomplete.util.math.enclosingBoundingBox
-import de.westnordost.streetcomplete.util.math.isCompletelyInside
+import io.ticofab.androidgpxparser.parser.GPXParser
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
@@ -63,13 +55,15 @@ class DisplaySettingsFragment :
     }
 
     private fun onClickDisplayGpxTrack() {
-        val gpxFileExists = context?.getExternalFilesDir(null)?.let { File(it, GPX_TRACK_FILE) }?.exists() == true
+        val gpxFileExists =
+            context?.getExternalFilesDir(null)?.let { File(it, GPX_TRACK_FILE) }?.exists() == true
         var d: AlertDialog? = null
         val selectFileButton = Button(context).apply {
             setText(R.string.pref_gpx_track_provide)
             val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
                 addCategory(Intent.CATEGORY_OPENABLE)
-                type = "application/octet-stream" // allows too many files, but application/gpx+xml doesn't work
+                type =
+                    "application/octet-stream" // allows too many files, but application/gpx+xml doesn't work
             }
             setOnClickListener {
                 d?.dismiss()
@@ -131,9 +125,14 @@ class DisplaySettingsFragment :
             }
         }
         try {
-            activity?.contentResolver?.openInputStream(uri)?.use { it.bufferedReader().use { reader ->
-                File(context?.getExternalFilesDir(null), GPX_TRACK_FILE).writeText(reader.readText())
-            } }
+            activity?.contentResolver?.openInputStream(uri)?.use {
+                it.bufferedReader().use { reader ->
+                    File(
+                        context?.getExternalFilesDir(null),
+                        GPX_TRACK_FILE
+                    ).writeText(reader.readText())
+                }
+            }
             gpx_track_changed = true
             onClickDisplayGpxTrack()
         } catch (e: IOException) {
@@ -166,20 +165,28 @@ fun loadGpxTrackPoints(context: Context, complain: Boolean = false): List<LatLon
     // <trkpt lat="..." lon="..."><ele>...</ele></trkpt>
     // <wpt lon="..." lat="...">
     val gpxFile = context.getExternalFilesDir(null)?.let { File(it, GPX_TRACK_FILE) }
+
     if (gpxFile?.exists() != true) {
         if (complain)
             context.toast(R.string.pref_gpx_track_loading_error, Toast.LENGTH_LONG)
         return null
     }
-    val gpxPoints = gpxFile.readLines().mapNotNull { line -> // this is a bit slow, but ok
-        val l = line.trim()
-        if (!l.startsWith("<trkpt") && !l.startsWith("<wpt")) return@mapNotNull null
-        val lat = l.substringAfter("lat=\"").substringBefore("\"").toDoubleOrNull()
-        val lon = l.substringAfter("lon=\"").substringBefore("\"").toDoubleOrNull()
-        if (lat == null || lon == null) null
-        else LatLon(lat, lon)
-    }
-    if (gpxPoints.size < 2) {
+
+    val gpxPoints = runCatching {
+        GPXParser().parse(gpxFile.inputStream()).tracks.map { track ->
+            track.trackSegments.map { segment ->
+                segment.trackPoints
+            }
+        }.flatten().flatten()
+            .map { trackPoint ->
+                LatLon(
+                    latitude = trackPoint.latitude,
+                    longitude = trackPoint.longitude
+                )
+            }
+    }.getOrNull()
+
+    if ((gpxPoints?.size ?: 0) < 2) {
         context.toast(R.string.pref_gpx_track_loading_error, Toast.LENGTH_LONG)
         return null
     }
