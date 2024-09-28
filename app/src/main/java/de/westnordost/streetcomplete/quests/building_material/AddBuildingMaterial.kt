f334883ad7271788c8b016d35092bf3944fdafd4
Alexis Lecanu <alexis.lecanu@gmail.com>
2024-09-28 21:42:04 +0200

    Fix quest

--- app/src/main/java/de/westnordost/streetcomplete/quests/building_material/AddBuildingMaterial.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/building_material/AddBuildingMaterial.kt
@@ -12,14 +12,14 @@ class AddBuildingMaterial : OsmFilterQuestType<BuildingMaterial>() {
           ((building and building !~ no|construction|roof|carport)
           or (building:part and building:part !~ no|construction|roof|carport))
           and !building:material
-          and (!indoor or indoor = no)
-          and wall !~ no
+          and indoor != no
+          and wall != no
     """
     override val changesetComment = "Specify building material"
     override val wikiLink = "Key:building:material"
     override val icon = R.drawable.ic_quest_building_material
 
-    override val defaultDisabledMessage = R.string.default_disabled_msg_buildingMaterial
+    override val defaultDisabledMessage = R.string.default_disabled_msg_expertise_needed
 
     override fun getTitle(tags: Map<String, String>) = when {
         tags.containsKey("building:part") -> R.string.quest_buildingPartMaterial_title
