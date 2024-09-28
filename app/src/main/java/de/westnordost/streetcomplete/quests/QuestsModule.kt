cdd12dbcd8449c06c23b371172b4f109c366e8d2
Alexis Lecanu <alexis.lecanu@gmail.com>
2024-09-28 21:49:29 +0200

    Fix quest value

--- app/src/main/java/de/westnordost/streetcomplete/quests/QuestsModule.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/QuestsModule.kt
@@ -609,7 +609,7 @@ fun getQuestTypeList(
     // quests added in SCEE
     EE_QUEST_OFFSET + 0 to AddBenchMaterial(),
     EE_QUEST_OFFSET + 27 to AddBuildingColour(),
-    EE_QUEST_OFFSET + 48 to AddBuildingMaterial(),
+    EE_QUEST_OFFSET + 49 to AddBuildingMaterial(),
     EE_QUEST_OFFSET + 24 to AddRoofColour(),
     EE_QUEST_OFFSET + 1 to AddContactPhone(),
     EE_QUEST_OFFSET + 2 to AddContactWebsite(),
